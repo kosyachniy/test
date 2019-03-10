e3254691f67a014e2772d7ff7434f6d63ce2cbd4
kosyachniy <polozhev@mail.ru>
2019-03-10 20:22:13 +0300

    Socket.IO

--- app/__init__.py
+++ app/__init__.py
@@ -1,18 +1,46 @@
 import json
 import requests
 
+URL = 'http://0.0.0.0:5000/'
+
 from flask import Flask
 app = Flask(__name__)
 
+import logging
+logging.basicConfig(filename='error.log',level=logging.DEBUG)
+
+from flask_socketio import SocketIO, emit
+async_mode = None
+socketio = SocketIO(app, async_mode=async_mode)
+
+script = '''
+<script src='/static/socket.io.min.js'></script>
+<script>
+namespace = 'main';
+var socket = io.connect(URL + namespace);
+
+window.setInterval(function() {
+    socket.emit('online', {id: 'Hi!'});
+}, 100);
+</script>
+'''
+
 @app.route("/", methods=["GET"])
 def get():
-    return "<h1 style='color:blue'>Hello There!</h1>"
+    return "<h1 style='color:blue'>Hello There!</h1>" + script
 
 @app.route("/post", methods=["GET"])
 def post():
-    res = requests.post('http://157.230.103.16:5000/sys').text
+    res = requests.post(URL + 'sys').text
     return res
 
 @app.route("/sys", methods=["POST"])
 def sys():
     return json.dumps({'123': 456, '789': 'str'})
+
+@socketio.on('online', namespace='/main')
+def sockio(mes):
+    print(mes)
+
+if __name__ == '__main__':
+	socketio.run(app, debug=True)
